4a3a58747f1a2d768f2b4a760e7a8c32aa7c965c
Pulkit Chandra <pulkitchandra89@gmail.com>
2021-07-10 02:59:48 +0530

    Updated configurations

---
+++ react-ssr/webpack.shared.config.js
@@ -0,0 +1,44 @@
+const ESLintPlugin = require("eslint-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(tsx|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  plugins: [
+    new ESLintPlugin(),
+    new HTMLWebpackPlugin({ template: "./public/index.html" }),
+    new MiniCssExtractPlugin(),
+  ],
+};
