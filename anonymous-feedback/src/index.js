fc4b9a97cc9357c732db269ff5aef42ffcef8896
Pulkit Chandra <pulkit.cs@gmail.com>
2021-10-21 23:06:12 +0530

    Initial commit for anonymous-feedback

---
+++ anonymous-feedback/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AnonymousFeedback from './anonymousFeedback';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AnonymousFeedback />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
