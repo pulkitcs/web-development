266c3d675e5a94439192256d0f24c7d66390e3bc
Pulkit Chandra <pulkitchandra89@gmail.com>
2023-03-21 12:12:43 +0530

    Server entry point

--- react-ssr/server/index.js
+++ react-ssr/server/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const { renderToString } = require("react-dom/server.js");
+const react = require("react");
+const pageTemplate = require("./defaultPage.js");
+const src = require('../dist/js/bundle-server.js');
 
 const app = express();
 
 const port = 9000;
 
 app.use(express.static('dist'));
+
+app.get("/home", (req, res) => {
+  const domString = renderToString(react.createElement(src.serverApp.default));
+  res.send(pageTemplate({
+    container: domString,
+  }));
+})
 
 app.listen(port, () => {
   console.log(`Server is live @ ${port}`);
